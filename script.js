c6339c147f66fcbef6f266ac17d7e4a21ff5a427
V31dy <71170989+V31dy@users.noreply.github.com>
2020-12-09 13:54:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -53,3 +53,24 @@ $(document).ready(function(){
 //добавляем ко всем обектам событие на запуск функции
 	$('.text_img').hover(showImg,showImgOff);
 })
+
+$(document).ready(function(){
+ /**
+ * При прокрутке страницы, показываем или срываем кнопку
+ */
+ $(window).scroll(function () {
+ // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+ if ($(this).scrollTop() > 50) {
+ $('#button-up').fadeIn();
+ } else {
+ $('#button-up').fadeOut();
+ }
+ });
+ /** При нажатии на кнопку мы перемещаемся к началу страницы */
+ $('#button-up').click(function () {
+ $('body,html').animate({
+ scrollTop: 0
+ }, 1000);
+ return false;
+ });
+});
